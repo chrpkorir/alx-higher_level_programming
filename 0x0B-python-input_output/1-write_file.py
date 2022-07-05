7411fcc0f31ada2c8a28db9353bd8757a9eb612a
chrpkorir <faythkoko@gmail.com>
2022-07-05 16:07:13 +0000

    Append to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,6 +8,6 @@ def number_of_lines(filename=""):
     """
     counts the number of lines in a file
     """
-    num_lines = 0
+    count = 0
     with open(filename, encoding="utf-8") as myFile:
         return myFile.read().count('\n')
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains the function
+read_lines
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+    Open and print nb_lines to std out
+    Read entire file if nb_lines <= 0 or nb_lines >= total lines
+    """
+    line = 1
+    with open(filename, encoding="utf-8", mode='r') as myFile:
+        line_count = myFile.read().count('\n')
+        myFile.seek(0)
+        if nb_lines <= 0 or nb_lines >= line_count:
+            print("{:s}".format(myFile.read()), end="")
+        else:
+            while line <= nb_lines:
+                print("{:s}".format(myFile.readline()), end="")
+                line += 1
