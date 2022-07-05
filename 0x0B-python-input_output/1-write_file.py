66b7129d43a032cd27afb98051d44ff381a27015
chrpkorir <faythkoko@gmail.com>
2022-07-05 16:04:27 +0000

    Writes to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains a function number_of_lines
+"""
+
+
+def number_of_lines(filename=""):
+    """
+    counts the number of lines in a file
+    """
+    num_lines = 0
+    with open(filename, encoding="utf-8") as myFile:
+        return myFile.read().count('\n')
