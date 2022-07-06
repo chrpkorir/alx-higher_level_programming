f4465cc717f71ba900b74dfaa75a350b4364143d
chrpkorir <faythkoko@gmail.com>
2022-07-06 10:03:30 +0000

    Add attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module 101-add_attribute.py.
+Checks if an attribute can be added to an object.
+"""
+
+def add_attribute(obj, attr, a_value):
+    """Checks if attr of value a_value can be added to obj.
+
+
+    Args:
+        - obj: object to add attribute to
+        - attr: attribute name
+        - a_value: value of attribute to be added
+    """
+
+    if not hasattr(obj, '__slots__') and not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    if hasattr(obj. '__slots__') and not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+
+    setattr(obj, attr, a_value):
