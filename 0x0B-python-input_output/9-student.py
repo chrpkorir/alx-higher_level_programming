c0dd8ea7492e03eefa2a591e348c836ae77e45e1
chrpkorir <faythkoko@gmail.com>
2022-07-06 12:29:45 +0000

    Class student that defines student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Retrieves a dictionary representation of a Student instance."""
+
+ def __init__(self, first_name, last_name, age):
+     """Retrieves a dictionary representation of a Student instance."""
+    return obj.__dict__
