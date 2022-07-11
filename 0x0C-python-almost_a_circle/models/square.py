fd7979126790ab27d1264787e2e184c0594f3cc8
chrpkorir <faythkoko@gmail.com>
2022-07-11 11:54:48 +0000

    Dictionary representation of a square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,3 +20,27 @@ class Square(Rectangle):
         """ Size setter"""
         self.width = size
         self.height = size
+
+     def __str__(self):
+        """ This function returns a string representation of the polygone"""
+        return("[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
+               self.id, self.x, self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """ This function updates values in the instance attributes"""
+        if args and args != "":
+            lista = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                if i < 5:
+                    setattr(self, lista[i], args[i])
+        else:
+            if kwargs and kwargs != "":
+                lista = ["id", "size", "x", "y"]
+                for key, value in kwargs.items():
+                    for i in range(len(lista)):
+                        if key == lista[i]:
+                            setattr(self, lista[i], value)
+
+    def to_dictionary(self):
+        """This function returns a dictionary representation of the square"""
+        return {'id': self.id, 'x': self.x, 'y': self.y, 'size': self.size}
