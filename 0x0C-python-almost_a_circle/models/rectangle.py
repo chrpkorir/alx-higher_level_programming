ba337baef87920d91e4e5c1392f0ef106cdd57dd
chrpkorir <faythkoko@gmail.com>
2022-07-11 09:57:14 +0000

    Added private instance attributes

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""This module creates a rectangle."""
+
+
+class Rectangle(Base):
+    """This is the class Rectangle
+    It inherits from base.
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Width getter."""
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Width setter."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Height getter."""
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Height setter."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """ x getter."""
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """ x setter."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter."""
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """ y setter."""
+        self.__y = value
+
