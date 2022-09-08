bdfae278a3534591c01af9f781de2d1f06215c52
chrpkorir <faythkoko@gmail.com>
2022-09-08 13:25:59 +0000

    Fix errors

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,17 +4,17 @@ if (process.argv.length < 4) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2);
-  let nextElement = Number.MIN_VALUE;
+  let secondElement = Number.MIN_VALUE;
   let maxValue = Number.MAX_VALUE;
 
   arr.forEach(function (element) {
     element = parseInt(element);
     if (element > maxValue) {
-      nextElement = maxValue;
+      secondElement = maxValue;
       maxValue = element;
-    } else if (nextElement < element && maxValue > element) {
-      nextElement = element;
+    } else if (secondElement < element && maxValue > element) {
+      secondElement = element;
     }
   });
-  console.log(nextElement);
+  console.log(secondElement);
 }
