513da6d2c94b5289550a239163f1432ff71d81f5
chrpkorir <faythkoko@gmail.com>
2022-09-22 16:32:33 +0000

    Added cURL POST parameters

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# script that takes in a URL, sends a POST request to the passed URL
+# and displays the response body
+curl "$1" -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
